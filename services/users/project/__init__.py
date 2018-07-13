0f79c133325e660f30aee024c972317b5534938a
jimmyjames <james.tappe@sendgrid.com>
2018-07-13 14:59:13 -0700

    debug toolbar and dont bind port 5432 for travis' sakes

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -2,9 +2,11 @@ import os
 import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
 
 # instantiate the db
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
 
 
 def create_app(script_info=None):
@@ -19,6 +21,7 @@ def create_app(script_info=None):
 
     # setup extensions
     db.init_app(app)
+    toolbar.init_app(app)
 
     # register blueprints
     from project.api.users import users_blueprint
--- services/users/project/tests/test_config.py
+++ services/users/project/tests/test_config.py
@@ -3,6 +3,7 @@ import unittest
 from flask import current_app
 from flask_testing import TestCase
 from project import create_app
+
 app = create_app()
 
 
@@ -15,6 +16,7 @@ class TestDevelopmentConfig(TestCase):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(current_app is None)
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL'))
+        self.assertTrue(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestTestingConfig(TestCase):
@@ -22,11 +24,12 @@ class TestTestingConfig(TestCase):
         app.config.from_object('project.config.TestingConfig')
         return app
 
-    def test_app_is_development(self):
+    def test_app_is_testing(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertTrue(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
-        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL'))
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_TEST_URL'))
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 class TestProductionConfig(TestCase):
@@ -34,11 +37,12 @@ class TestProductionConfig(TestCase):
         app.config.from_object('project.config.ProductionConfig')
         return app
 
-    def test_app_is_development(self):
+    def test_app_is_production(self):
         self.assertTrue(app.config['SECRET_KEY'] == 'my_precious')
         self.assertFalse(app.config['TESTING'])
         self.assertFalse(app.config['PRESERVE_CONTEXT_ON_EXCEPTION'])
         self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == os.environ.get('DATABASE_URL'))
+        self.assertFalse(app.config['DEBUG_TB_ENABLED'])
 
 
 if __name__ == '__main__':
