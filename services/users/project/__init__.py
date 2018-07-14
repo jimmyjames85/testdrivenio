e3941aca66ba88dbdae25f92c424d567feab2375
jimmyjames <james.tappe@sendgrid.com>
2018-07-14 14:31:32 -0700

    react

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -3,6 +3,7 @@ import sys
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS
 
 # instantiate the db
 db = SQLAlchemy()
@@ -13,6 +14,9 @@ def create_app(script_info=None):
 
     # instantiate the app
     app = Flask(__name__)
+
+    # enable CORS
+    CORS(app)
 
     # set config
     app_settings = os.getenv('APP_SETTINGS')
