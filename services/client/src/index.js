388a988401e2da4ddf63a7eb9fbb093412ab9384
jimmyjames <james.tappe@sendgrid.com>
2018-07-14 15:30:13 -0700

    react forms

--- services/client/src/index.js
+++ services/client/src/index.js
@@ -2,13 +2,18 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
 import UsersList from './components/UsersList';
+import AddUser from './components/AddUser';
 
 class App extends Component {
     constructor(){
 	super();
 	this.state = {
-	    users: []
+	    users: [],
+	    username: '',
+	    email: '',
 	};
+	this.addUser = this.addUser.bind(this);
+	this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount(){
@@ -23,6 +28,13 @@ class App extends Component {
 	      <br/>
 	      <h1>All Users</h1>
 	      <hr/><br/>
+		<AddUser
+		  username={this.state.username}
+		  email={this.state.email}
+		  addUser={this.addUser}
+		  handleChange={this.handleChange}
+		/>
+	      <br/>
 		<UsersList users={this.state.users}/>
 	    </div>
 	  </div>
@@ -34,6 +46,26 @@ class App extends Component {
 	axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
 	    .then((res) => { this.setState({ users: res.data.data.users }); })
 	    .catch((err) => { console.log(err); });
+    }
+
+    addUser(event) {
+	event.preventDefault();
+	const data = {
+	    username: this.state.username,
+	    email: this.state.email
+	};
+	axios.post(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`, data)
+	    .then((res) => {
+		this.getUsers();
+		this.setState({ username: '', email: ''});
+	    })
+	    .catch((err) => { console.log(err); });
+    }
+
+    handleChange(event) {
+	const obj = {};
+	obj[event.target.name] = event.target.value;
+	this.setState(obj);
     }
 
 };
