e7bd869e6c4fb14b07d76c0e3fbc2dd402f1661f
jimmyjames <james.tappe@sendgrid.com>
2018-07-15 16:48:12 -0700

    new form component; table driven tests

--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -3,6 +3,8 @@ import axios from 'axios';
 import UsersList from './components/UsersList';
 import AddUser from './components/AddUser';
 import About from './components/About';
+import NavBar from './components/NavBar';
+import Form from './components/Form';
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
@@ -12,6 +14,12 @@ class App extends Component {
 	    users: [],
 	    username: '',
 	    email: '',
+	    title: 'TestDriven.io',
+	    formData: {
+		username: '',
+		email: '',
+		password: ''
+	    }
 	};
 	this.addUser = this.addUser.bind(this);
 	this.handleChange = this.handleChange.bind(this);
@@ -23,27 +31,44 @@ class App extends Component {
 
     render(){
 	return (
-	<div className="container">
-	  <div className="row">
-	    <div className="col-md-6">
-	      <br/>
-	      <Switch>
-		<Route exact path='/' render={() => (
-		  <div>
-		    <h1>All Users</h1>
-		    <hr/><br/>
-		    <AddUser
-		      username={this.state.username}
-		      email={this.state.email}
-		      addUser={this.addUser}
-		      handleChange={this.handleChange}
-		    />
-		    <br/>
-		    <UsersList users={this.state.users}/>
-		  </div>
-		)} />
-		<Route exact path='/about' component={About}/>
-	      </Switch>
+	<div>
+	  <NavBar title={this.state.title} />
+	  <div className="container">
+	    <div className="row">
+	      <div className="col-md-6">
+		<br/>
+		<Switch>
+		  <Route exact path='/' render={() => (
+		    <div>
+		      <h1>All Users</h1>
+		      <hr/><br/>
+		      <AddUser
+			username={this.state.username}
+			email={this.state.email}
+			addUser={this.addUser}
+			handleChange={this.handleChange}
+		      />
+		      <br/>
+		      <UsersList users={this.state.users}/>
+		    </div>
+		    )} />
+		  <Route exact path='/about' component={About}/>
+		  <Route exact path='/register' render={() => (
+		      <Form
+		        formType={'Register'}
+		        formData={this.state.formData}
+		      />
+		  )}/>
+		  <Route exact path='/login' render={() => (
+		      <Form
+		        formType={'Login'}
+		        formData={this.state.formData}
+		      />
+		  )}/>
+
+
+		</Switch>
+	      </div>
 	    </div>
 	  </div>
 	</div>
