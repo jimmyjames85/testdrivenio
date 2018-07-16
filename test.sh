09c52ae82f9c6c84c9d850a68bfeefeb8a7ba8b9
jimmyjames <james.tappe@sendgrid.com>
2018-07-15 21:44:49 -0700

    e2e... but seriously when TF do I get to play with AWS stuffs

--- test.sh
+++ test.sh
@@ -37,6 +37,11 @@ inspect $? users-lint
 if [[ "${env}" == "dev" ]]; then
     docker-compose -f $file run client npm test -- --coverage
     inspect $? client
+    testcafe chrome e2e
+    inspect $? e2e
+else
+    testcafe chrome e2e/index.test.js
+    inpect $? e2e
 fi
 
 
