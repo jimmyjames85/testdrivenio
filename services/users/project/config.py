dcd21122c374eeddc490ec50e6c742f7a857d7e6
jimmyjames <james.tappe@sendgrid.com>
2018-07-31 16:29:01 -0700

    make the python linter happy I guess

--- services/users/project/config.py
+++ services/users/project/config.py
@@ -29,9 +29,11 @@ class TestingConfig(BaseConfig):
     TOKEN_EXPIRATION_DAYS = 0
     TOKEN_EXPIRATION_SECONDS = 3
 
+
 class StagingConfig(BaseConfig):
     """Staging configuration"""
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+
 
 class ProductionConfig(BaseConfig):
     """Production Configuration"""
