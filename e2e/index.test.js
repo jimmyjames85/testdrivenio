cb4366178c3ff7be926c9edeb88b766847afffe6
jimmyjames <james.tappe@sendgrid.com>
2018-07-31 13:04:31 -0700

    React Flash Messaging

--- e2e/index.test.js
+++ e2e/index.test.js
@@ -5,7 +5,6 @@ const TEST_URL = process.env.TEST_URL;
 fixture('/').page(`${TEST_URL}/`);
 
 test(`should display the page correctly if a user is not logged in`, async (t) => {
-
     await t
 	.navigateTo(TEST_URL)
 	.expect(Selector('H1').withText('All Users').exists).ok()
@@ -13,4 +12,5 @@ test(`should display the page correctly if a user is not logged in`, async (t) =
 	.expect(Selector('a').withText('Log Out').exists).notOk()
 	.expect(Selector('a').withText('Register').exists).ok()
 	.expect(Selector('a').withText('Log In').exists).ok()
+	.expect(Selector('.alert').exists).notOk()
 });
--- services/client/src/App.jsx
+++ services/client/src/App.jsx
@@ -7,39 +7,70 @@ import NavBar from './components/NavBar';
 import Form from './components/forms/Form';
 import Logout from './components/Logout';
 import UserStatus from './components/UserStatus';
+import Message from './components/Message';
+
 class App extends Component {
+
     constructor() {
 	super();
 	this.state = {
 	    users: [],
 	    title: 'TestDriven.io',
-	    isAuthenticated: false
+	    isAuthenticated: false,
+	    messageName: null,
+	    messageType: null,
 	};
 	this.logoutUser = this.logoutUser.bind(this);
-	this.loginUser = this.loginUser.bind(this)
+	this.loginUser = this.loginUser.bind(this);
+	this.createMessage = this.createMessage.bind(this);
+	this.removeMessage = this.removeMessage.bind(this);
     }
+
     componentWillMount() {
 	if (window.localStorage.getItem('authToken')) {
 	    this.setState({ isAuthenticated: true });
 	};
     };
+
     componentDidMount() {
 	this.getUsers();
     };
+
     getUsers() {
 	axios.get(`${process.env.REACT_APP_USERS_SERVICE_URL}/users`)
 	.then((res) => { this.setState({ users: res.data.data.users }); })
 	.catch((err) => { });
     };
+
     logoutUser() {
 	window.localStorage.clear();
 	this.setState({ isAuthenticated: false });
     };
+
     loginUser(token) {
 	window.localStorage.setItem('authToken', token);
 	this.setState({ isAuthenticated: true });
 	this.getUsers();
+	this.createMessage('Welcome!', 'success');
     };
+
+    createMessage(name='Sanity Check', type='success') {
+	this.setState({
+	    messageName: name,
+	    messageType: type
+	});
+	setTimeout(() => {
+	    this.removeMessage();
+	}, 3000);
+    };
+
+    removeMessage() {
+	this.setState({
+	    messageName: null,
+	    messageType: null
+	});
+    };
+
     render() {
 	return (
 	    <div>
@@ -48,6 +79,13 @@ class App extends Component {
 	    isAuthenticated={this.state.isAuthenticated}
 	    />
 	    <div className="container">
+	    {this.state.messageName && this.state.messageType &&
+		<Message
+		messageName={this.state.messageName}
+		messageType={this.state.messageType}
+		removeMessage={this.removeMessage}
+		/>
+	    }
 	    <div className="row">
 	    <div className="col-md-6">
 	    <br/>
@@ -63,6 +101,7 @@ class App extends Component {
 		formType={'register'}
 		isAuthenticated={this.state.isAuthenticated}
 		loginUser={this.loginUser}
+		createMessage={this.createMessage}
 		/>
 	    )} />
 	    <Route exact path='/login' render={() => (
@@ -70,6 +109,7 @@ class App extends Component {
 		formType={'login'}
 		isAuthenticated={this.state.isAuthenticated}
 		loginUser={this.loginUser.bind(this)}
+		createMessage={this.createMessage}
 		/>
 	    )} />
 	    <Route exact path='/logout' render={() => (
